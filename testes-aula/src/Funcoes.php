dc67adf3454d92bbf433b6cc93cbf895b97bb18b
Johnny Matheus <alunotioficial@gmail.com>
2025-04-07 16:02:33 -0300

    Configurando o projeto

---
+++ testes-aula/src/Funcoes.php
@@ -0,0 +1,19 @@
+<?php
+
+class Funcoes{
+    public static function isEven($n){
+        return $n % 2  ===  0;
+    }
+
+
+    public static function factorial($n){
+        if()  
+    
+    
+    }
+}
+
+
+
+
+?>
